25af6ab24078cdb075e0c076ddac246f8c72eb2a
immedeg <145657237+immedeg@users.noreply.github.com>
2025-10-30 20:15:56 +0300

    Updates

---
+++ assets/main-BqzY1_3a.js
@@ -0,0 +1,1 @@
+import"./style-CuoPheRG.js";console.log("Добро пожаловать в приложение Факты и Картинки!");document.addEventListener("DOMContentLoaded",function(){console.log("Страница загружена!")});
